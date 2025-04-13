f2896e6f3de4d321579e5c92b022355b75b2b04b
ISHA <ishatripathi237@gmail.com>
2025-04-13 12:47:48 +0530

    Prepare for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://lexinco-frontend.onrender.com'],
+    origin: ['http://localhost:3000', 'https://lexinco.herokuapp.com'],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
