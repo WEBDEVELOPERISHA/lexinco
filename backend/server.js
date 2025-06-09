9b9cd1a7c5502473f5db42b9ce48830eb54589b1
Isha_Tripathi <isha.tripathi@trucsr.in>
2025-06-09 11:11:34 +0530

    Add nodemailer and fix server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const Razorpay = require('razorpay');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
